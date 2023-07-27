1e36de15d69a19ffedfd2cf819d6f5fcd74bff61
harsh4786 <50767810+harsh4786@users.noreply.github.com>
2023-07-28 02:13:41 +0530

    refactor

--- benches/benchmerkle.rs
+++ benches/benchmerkle.rs
@@ -6,15 +6,17 @@ use solana_merkle_tree::MerkleTree;
 
 fn bench_merkelization(bench: &mut Bench){
     bench.task("merkelization", |task|{
+        let rng = &mut rand::thread_rng();
+        let recs = Receipt::gen_random_nodes(rng);
+        println!("FOR {:?} SAMPLES \n", recs.len());
+        
         task.iter(||{
-            let rng = &mut rand::thread_rng();
-            let recs = Receipt::gen_random_nodes(rng);
             MerkleTree::new(recs.as_slice())
         })
     })
 }
 
 glassbench!(
-    "merkle bench",
+    "merkle brench",
     bench_merkelization,
 );
